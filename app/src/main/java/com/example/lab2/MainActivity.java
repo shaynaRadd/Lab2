c072c17fa336c03fd9f808509bcc27fa8656b88c
shaynaRadd <radd0013@algonquinlive.com>
2019-09-16 15:35:23 -0400

    Initial commit

---
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
+    }
+}
