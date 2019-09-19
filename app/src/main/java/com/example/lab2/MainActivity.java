421c88ec1c133a99066252e0e8e50ecc76ca1a40
shaynaRadd <radd0013@algonquinlive.com>
2019-09-19 12:35:05 -0400

    Final formatting

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -10,8 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_main);
-        //setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_linear);
         //setContentView(R.layout.activity_main_grid);
-        setContentView(R.layout.activity_main_relative);
+        //setContentView(R.layout.activity_main_relative);
     }
 }
